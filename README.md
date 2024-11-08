2a1f59a2ab9ab70fbf6428369cee3f7c2ba410d7
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2024-11-08 11:00:46 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# `sae_course` version 0.1.0
+
+This is an example repository useful for trying things out in git.
+
+### Revision control
+
+The python code and this README file are revisioned using GitHub here:
+
+https://github.com/xapple/sae_course
---
+++ scratch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Development script to test some of the code in the python module
+and try out different things. This script can safely be ignored
+and is meant simply as a sandbox.
+"""
+
+# Message #
+print("Hello world...")
